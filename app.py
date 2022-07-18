62226c86452837895e78bdd9984bc3b5ce4bfb0c
Shaheen Rahimani <shaheensr@outlook.com>
2022-07-18 15:11:59 -0300

    Added imports needed to implement login

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    logout_user,
+    login_user,
+    current_user,
+    login_required,
+)
+import os
+import json
 
 app = flask.Flask(__name__)
 
