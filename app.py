8e58047967d51e9a837f381a64788fdcb0ed0dfa
Shaheen Rahimani <shaheensr@outlook.com>
2022-07-18 15:23:04 -0300

    Added database and routes template

--- app.py
+++ app.py
@@ -13,8 +13,55 @@ import json
 
 app = flask.Flask(__name__)
 
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+db = SQLAlchemy(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+
+db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route("/")
 def home():
-    return "Hello World"
+    return flask.render_template("index.html")
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+''' @app.route("/login", methods = ['POST'])
+def login_post():
+    #Add login logic here '''
+
+@app.route("register")
+def register():
+    flask.render_template("register.html")
+
+''' @app.route("/register", methods = ['POST'])
+def register_post():
+    Add registration code here '''
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return flask.redirect(flask.url_for("index"))
 
 app.run()
